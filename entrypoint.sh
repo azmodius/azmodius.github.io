b6dc42230ac3d0cd39876b58885470d81ccb3d10
azmodius <oni.azmodius@gmail.com>
2021-03-07 22:48:07 -0800

    add some header pages and socials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash 
 
-bundle install && bundle update github-pages && bundle update && bundle exec jekyll serve --watch --incremental --config _config.yml --host 0.0.0.0
+echo "Running bundle install..."
+bundle install
+
+echo "Running bundle update github-pages"
+bundle update github-pages
+
+echo "Running bundle update..."
+bundle update
+
+echo "Starting jekyll..."
+bundle exec jekyll serve --watch --incremental --config _config.yml --host 0.0.0.0
--- index.md
+++ index.md
@@ -1,1 +1,2 @@
-## Hi! I'm Johan!
+## Home
+Howdy fam!
